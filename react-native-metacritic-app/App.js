bec2f55fea0d9a5471778354eddb565b91286f31
Luis Monasterios <ljmm2k2@gmail.com>
2025-01-14 19:58:31 -0400

    feat: Agregar imagen de icono y actualizar estilos en App.js

--- react-native-metacritic-app/App.js
+++ react-native-metacritic-app/App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require("./assets/icon.png")}
+        style={{ width: 100, height: 100 }}
+      />
+      <Text>Open up App.js !</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#09f",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
